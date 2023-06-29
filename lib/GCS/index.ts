4d5d950638535e96098dfbbe1a76f2179d597768
Deston Bui <khangduy158@gmail.com>
2023-06-29 22:34:37 +0700

    Fix - GCS - Change unnecessary streaming upload to appropriate uploadFromMemory

--- lib/GCS/index.ts
+++ lib/GCS/index.ts
@@ -1,40 +1,39 @@
 import { Storage } from "@google-cloud/storage";
-import stream from "stream";
-
-const storage = new Storage({
-  projectId: process.env.GCS_PROJECT_ID,
-  credentials: {
-    client_email: process.env.GCS_KEY_CLIENT_EMAIL,
-    private_key: process.env.GCS_KEY_PRIVATE_KEY,
-  },
-});
-
-const bucket = storage.bucket(process.env.GCS_BUCKET_ID as string);
 
 export async function uploadImage(imgBlob: Blob, filename: string) {
+  const storage = new Storage({
+    projectId: process.env.GCS_PROJECT_ID,
+    credentials: {
+      client_email: process.env.GCS_KEY_CLIENT_EMAIL,
+      private_key: process.env.GCS_KEY_PRIVATE_KEY,
+    },
+  });
+
+  const bucketName = process.env.GCS_BUCKET_ID;
+
   const buffer = Buffer.from(await imgBlob.arrayBuffer());
 
-  const file = bucket.file(filename);
+  async function uploadFromMemory() {
+    try {
+      await storage.bucket(bucketName).file(filename).save(buffer);
 
-  const passthroughStream = new stream.PassThrough();
-  passthroughStream.write(buffer);
-  passthroughStream.end();
-
-  async function streamFileUpload() {
-    passthroughStream.pipe(file.createWriteStream()).on("finish", () => {});
+      return { ok: true };
+    } catch (error) {
+      console.log(error);
+      return {
+        error:
+          "Something went wrong while uploading file, please check your console.",
+      };
+    }
   }
 
-  try {
-    await streamFileUpload();
+  const { ok, error } = await uploadFromMemory();
 
-    return JSON.stringify({
-      ok: true,
-      filepath: `https://storage.googleapis.com/pl-nextjs13-bucket/${filename}`,
-    });
-  } catch (error) {
-    return JSON.stringify({
-      ok: false,
-      error: error,
-    });
+  if (error) {
+    return { error };
   }
+
+  return {
+    filepath: `https://storage.googleapis.com/pl-nextjs13-bucket/${filename}`,
+  };
 }

e48ef91f727516b8fcea9e9a281cf24643ecef1f
Deston Bui <khangduy158@gmail.com>
2023-07-06 19:40:27 +0700

    Fix - vercel deploy error

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,7 +5,6 @@ import { Metadata } from "next";
 import HeroCarousel from "@/lib/components/section/HeroCarousel";
 
 import CategoriesNav from "@/lib/components/section/CategoriesNav";
-import { Category } from "@prisma/client";
 
 interface HomeProps {}
 
@@ -15,34 +14,28 @@ export const metadata: Metadata = {
     "Trải qua hơn 50 năm chắt chiu tinh hoa từ những búp trà xanh và hạt cà phê thượng hạng cùng mong muốn mang lại cho khách hàng những trải nghiệm giá trị nhất khi thưởng thức.",
 };
 
-async function fetchData() {
-  const fetchBanners = fetch(process.env.HOST + "/api/banners", {
+async function fetchBanners() {
+  const endpoint = process.env.HOST + "/api/banners";
+
+  const res = await fetch(endpoint, {
     next: { revalidate: 60 },
   });
 
-  const fetchCategories = fetch(process.env.HOST + "/api/categories", {
-    next: { revalidate: 60 },
-  });
+  if (!res.ok) {
+    throw new Error("Fetch banners failed");
+  }
 
-  const [bannersData, categoriesData] = await Promise.all([
-    fetchBanners,
-    fetchCategories,
-  ]);
-
-  const banners = await bannersData.json();
-  const categories = await categoriesData.json();
-
-  return { banners, categories };
+  return res.json();
 }
 
 const Home = async ({}: HomeProps) => {
-  const { banners, categories } = await fetchData();
+  const banners = await fetchBanners();
 
   return (
     <>
       <HeroCarousel items={banners} />
 
-      <CategoriesNav categories={categories} />
+      {/* <CategoriesNav /> */}
     </>
   );
 };

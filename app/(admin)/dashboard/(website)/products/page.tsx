6d49ffbe735e55c241dc96fe8083fc9ef2f3532c
Deston Bui <khangduy158@gmail.com>
2023-07-16 20:34:00 +0700

    Add - Products display

--- app/(admin)/dashboard/(website)/products/page.tsx
+++ app/(admin)/dashboard/(website)/products/page.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 
 import AddProductButton from "@/lib/components/ui/products/AddProductButton";
+import { actionFetchProducts } from "./actions";
+import Image from "next/image";
 
 interface Props {}
 
 const Products = async (props: Props) => {
-  // const { categories, error } = await actionFetchCategories();
-
-  // if (error) {
-  //   throw new Error("Fetch categories failed");
-  // }
+  const { products } = await actionFetchProducts();
 
   return (
     <>
@@ -26,7 +24,33 @@ const Products = async (props: Props) => {
 
       <hr className="my-2 border-gray-300" />
 
-      {/*  */}
+      {/* Products display */}
+      <div className="flex flex-wrap gap-2">
+        {products?.map((product) => {
+          return (
+            <div
+              key={product.name}
+              className="flex w-[200px] flex-col rounded-md bg-white p-4 shadow"
+            >
+              <div className="flex justify-center py-4">
+                <Image
+                  src={product.image_url}
+                  alt={product.name}
+                  width={150}
+                  height={150}
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <h6 className="mt-4 text-lg font-semibold text-theme-green-main">
+                  {product.name}
+                </h6>
+                <span>{product.price}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
